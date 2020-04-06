7ed234b769b945beca8b0b4e370e2e74ea02ea45
OKayTee <zsoltiokt@gmail.com>
2020-04-06 10:23:13 +0300

    changed from HSV to HSL

--- Camctrl.py
+++ Camctrl.py
@@ -16,21 +16,21 @@ def recog():
     
     pix = im.load()
     values = [[0 for a in range(3)] for b in range (100)]
-    valueshsv = [[0.0 for a in range(3)] for b in range(100)]
+    valueshsl = [[0.0 for a in range(3)] for b in range(100)]
     z=0
     for x in range (10):
         for y in  range(10):
             
             values[z][0], values[z][1], values[z][2] = pix[(x+236), (y+330)]
-            valueshsv[z][0], valueshsv[z][1], valueshsv[z][2] = colorsys.rgb_to_hsv((values[z][0]/255),(values[z][1]/255),(values[z][2]/255))
-            valueshsv[z][0] = valueshsv[z][0]*360
-            valueshsv[z][1] = valueshsv[z][1]*100
-            valueshsv[z][2] = valueshsv[z][2]*100
+            valueshsl[z][0], valueshsl[z][1], valueshsl[z][2] = colorsys.rgb_to_hls((values[z][0]/255),(values[z][1]/255),(values[z][2]/255))
+            valueshsl[z][0] = valueshsl[z][0]*360
+            valueshsl[z][1] = valueshsl[z][1]*100
+            valueshsl[z][2] = valueshsl[z][2]*100
             z=z+1
     r,g,b = geomean(values)
-    h,s,v = geomean(valueshsv)
+    h,s,l = geomean(valueshsl)
     print(r," ", g, " ", b)
-    print(h," ",s," ", v)
+    print(h," ",s," ", l)
 
 def getState():
     output = 'FULDUDRDRUFBFRLURDFLBUFRRBBBDLLDULLRLRUBLBDBDDFUFBRFUF'
