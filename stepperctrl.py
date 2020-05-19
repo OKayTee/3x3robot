76c7aefb0cefff14ebdccc0c94e8daf5a17033ec
OKayTee <zsoltiokt@gmail.com>
2020-05-19 11:14:04 +0300

    changed to double

--- stepperctrl.py
+++ stepperctrl.py
@@ -63,97 +63,97 @@ def move(algorithm):
             down(1)
         elif moves == "[":
             down(2)
-    
+        time.sleep(0.5)
 
 def up(ang):
     if ang == 0:
-        for i in range(100):
-            kit1.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit1.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
         
     elif ang == 1:
-        for i in range(100):
-            kit1.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit1.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
         
     elif ang == 2:
-        for i in range(200):
-            kit1.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit1.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit1.stepper1.release()
 
 def down(ang):
     if ang == 0:
-        for i in range(100):
-            kit1.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit1.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 1:
-        for i in range(100):
-            kit1.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit1.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 2:
-        for i in range(200):
-            kit1.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit1.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit1.stepper2.release()  
 
 def front(ang):
     if ang == 0:
-        for i in range(100):
-            kit2.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit2.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 1:
-        for i in range(100):
-            kit2.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit2.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 2:
-        for i in range(200):
-            kit2.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit2.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit2.stepper1.release()
 
 def behind(ang):
     if ang == 0:
-        for i in range(100):
-            kit2.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit2.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 1:
-        for i in range(100):
-            kit2.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit2.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 2:
-        for i in range(200):
-            kit2.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit2.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit2.stepper2.release()
 
 def left(ang):
     if ang == 0:
-        for i in range(100):
-            kit3.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit3.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 1:
-        for i in range(100):
-            kit3.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit3.stepper1.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 2:
-        for i in range(200):
-            kit3.stepper1.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit3.stepper1.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit3.stepper1.release()
 
 def right(ang):
     if ang == 0:
-        for i in range(100):
-            kit3.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit3.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 1:
-        for i in range(100):
-            kit3.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.INTERLEAVE)
+        for i in range(51):
+            kit3.stepper2.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
             time.sleep(0.01)
     elif ang == 2:
-        for i in range(200):
-            kit3.stepper2.onestep(style = stepper.INTERLEAVE)
+        for i in range(101):
+            kit3.stepper2.onestep(style = stepper.DOUBLE)
             time.sleep(0.01)
     kit3.stepper2.release()
     
